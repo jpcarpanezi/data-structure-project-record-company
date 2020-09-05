4c3d061093c659e0d691bebceaa78792755ea19d
Murilo Jacon <m.jacon@aluno.ifsp.edu.br>
2020-09-05 16:04:41 -0300

    Classes Album, Artista e Cancao

---
+++ Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structure_project_record_company
+{
+    class Album {
+        public int Codigo { get; set; }
+        public string Titulo { get; set; }
+        public int[] CodigosMusicas { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public long NumeroCopiasVendidas { get; set; }
+        public string[] ArtistasParticipantes { get; set; }
+    }
+}
