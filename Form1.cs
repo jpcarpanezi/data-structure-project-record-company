edaae7c4485174e667208e2febd5f48013121d30
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2020-09-05 15:30:29 -0300

    Added: general.cs

--- Form1.cs
+++ Form1.cs
@@ -9,13 +9,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace data_structure_project_record_company{
-    public partial class Form1 : Form{
-        public Form1(){
-            InitializeComponent();
-        }
+	public partial class Form1 : Form{
+		public Form1(){
+			InitializeComponent();
+		}
 
-        private void Form1_Load(object sender, EventArgs e){
+		private void Form1_Load(object sender, EventArgs e){
 
-        }
-    }
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			general.mandarMuriloTomarNoCu("Murilo, poderia por obséquio enfiar algo de natureza grotesca no seu orificio anal, seu filho de uma dama que por ventura realiza casos extraconjugais remunerados.");
+		}
+	}
 }
---
+++ general.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Windows.Forms;
+
+namespace data_structure_project_record_company {
+	class general {
+		public static void mandarMuriloTomarNoCu(String message){
+			MessageBox.Show(message);
+		}
+	}
+}
