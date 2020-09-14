097745ae5d97b2ff8c0b1ad203ab043e256d68df
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2020-09-14 14:47:40 -0300

    Finished: Search.cs

--- Search.Designer.cs
+++ Search.Designer.cs
@@ -23,52 +23,137 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
+			this.tipoBusca = new System.Windows.Forms.ComboBox();
+			this.codeField = new System.Windows.Forms.TextBox();
+			this.searchButton = new System.Windows.Forms.Button();
+			this.searchDefine = new System.Windows.Forms.Label();
+			this.editButton = new System.Windows.Forms.Button();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.dataGridLabel1 = new System.Windows.Forms.Label();
+			this.dataGridLabel2 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// comboBox1
+			// tipoBusca
 			// 
-			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Items.AddRange(new object[] {
-            "",
+			this.tipoBusca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.tipoBusca.FormattingEnabled = true;
+			this.tipoBusca.Items.AddRange(new object[] {
             "Artista",
             "Album",
             "Canção"});
-			this.comboBox1.Location = new System.Drawing.Point(12, 12);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(121, 23);
-			this.comboBox1.TabIndex = 0;
+			this.tipoBusca.Location = new System.Drawing.Point(12, 12);
+			this.tipoBusca.Name = "tipoBusca";
+			this.tipoBusca.Size = new System.Drawing.Size(121, 23);
+			this.tipoBusca.TabIndex = 0;
 			// 
-			// textBox1
+			// codeField
 			// 
-			this.textBox1.Location = new System.Drawing.Point(139, 12);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.PlaceholderText = "Digite o código";
-			this.textBox1.Size = new System.Drawing.Size(568, 23);
-			this.textBox1.TabIndex = 1;
+			this.codeField.Location = new System.Drawing.Point(139, 12);
+			this.codeField.Name = "codeField";
+			this.codeField.PlaceholderText = "Digite o código";
+			this.codeField.Size = new System.Drawing.Size(568, 23);
+			this.codeField.TabIndex = 1;
 			// 
-			// button1
+			// searchButton
 			// 
-			this.button1.Location = new System.Drawing.Point(713, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "Pesquisar";
-			this.button1.UseVisualStyleBackColor = true;
+			this.searchButton.Location = new System.Drawing.Point(713, 12);
+			this.searchButton.Name = "searchButton";
+			this.searchButton.Size = new System.Drawing.Size(75, 23);
+			this.searchButton.TabIndex = 2;
+			this.searchButton.Text = "Pesquisar";
+			this.searchButton.UseVisualStyleBackColor = true;
+			this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+			// 
+			// searchDefine
+			// 
+			this.searchDefine.AutoSize = true;
+			this.searchDefine.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.searchDefine.Location = new System.Drawing.Point(12, 55);
+			this.searchDefine.Name = "searchDefine";
+			this.searchDefine.Size = new System.Drawing.Size(75, 21);
+			this.searchDefine.TabIndex = 3;
+			this.searchDefine.Text = "Null: Null";
+			this.searchDefine.Visible = false;
+			// 
+			// editButton
+			// 
+			this.editButton.Location = new System.Drawing.Point(713, 55);
+			this.editButton.Name = "editButton";
+			this.editButton.Size = new System.Drawing.Size(75, 23);
+			this.editButton.TabIndex = 4;
+			this.editButton.Text = "Editar Artista";
+			this.editButton.UseVisualStyleBackColor = true;
+			this.editButton.Visible = false;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AllowUserToAddRows = false;
+			this.dataGridView2.AllowUserToDeleteRows = false;
+			this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(12, 365);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(776, 150);
+			this.dataGridView2.TabIndex = 6;
+			this.dataGridView2.Text = "dataGridView2";
+			this.dataGridView2.Visible = false;
+			// 
+			// dataGridLabel1
+			// 
+			this.dataGridLabel1.AutoSize = true;
+			this.dataGridLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.dataGridLabel1.Location = new System.Drawing.Point(12, 114);
+			this.dataGridLabel1.Name = "dataGridLabel1";
+			this.dataGridLabel1.Size = new System.Drawing.Size(49, 21);
+			this.dataGridLabel1.TabIndex = 7;
+			this.dataGridLabel1.Text = "NULL";
+			this.dataGridLabel1.Visible = false;
+			// 
+			// dataGridLabel2
+			// 
+			this.dataGridLabel2.AutoSize = true;
+			this.dataGridLabel2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.dataGridLabel2.Location = new System.Drawing.Point(12, 341);
+			this.dataGridLabel2.Name = "dataGridLabel2";
+			this.dataGridLabel2.Size = new System.Drawing.Size(49, 21);
+			this.dataGridLabel2.TabIndex = 8;
+			this.dataGridLabel2.Text = "NULL";
+			this.dataGridLabel2.Visible = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 138);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+			this.dataGridView1.TabIndex = 5;
+			this.dataGridView1.Text = "dataGridView1";
+			this.dataGridView1.Visible = false;
 			// 
 			// Search
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.comboBox1);
+			this.ClientSize = new System.Drawing.Size(800, 630);
+			this.Controls.Add(this.dataGridLabel2);
+			this.Controls.Add(this.dataGridLabel1);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.editButton);
+			this.Controls.Add(this.searchDefine);
+			this.Controls.Add(this.searchButton);
+			this.Controls.Add(this.codeField);
+			this.Controls.Add(this.tipoBusca);
 			this.Name = "Search";
 			this.Text = "Search";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -76,8 +161,14 @@
 
 		#endregion
 
-		private System.Windows.Forms.ComboBox comboBox1;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox tipoBusca;
+		private System.Windows.Forms.TextBox codeField;
+		private System.Windows.Forms.Button searchButton;
+		private System.Windows.Forms.Label searchDefine;
+		private System.Windows.Forms.Button editButton;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Label dataGridLabel1;
+		private System.Windows.Forms.Label dataGridLabel2;
 	}
 }
--- Search.cs
+++ Search.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,6 +11,199 @@ namespace data_structure_project_record_company {
 	public partial class Search : Form {
 		public Search() {
 			InitializeComponent();
+		}
+
+		private int binarySearchResult;
+
+		private void searchButton_Click(object sender, EventArgs e) {
+			string erro = "";
+
+			if (!int.TryParse(codeField.Text, out int codigo))
+				erro += "- O código precisa ser um número\n";
+
+			if (codigo <= 0)
+				erro += "- O código precisa ser maior que 0\n";
+
+			if (codeField.Text == "")
+				erro += "- O campo de código não pode ser nulo\n";
+
+			if (tipoBusca.SelectedIndex == -1){
+				erro += "- Você precisa selecionar o tipo de busca\n";
+			}
+
+			if (erro != "") {
+				MessageBox.Show(erro, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			editButton.Click -= editArtista;
+			editButton.Click -= editAlbum;
+			editButton.Click -= editCancao;
+
+			switch (tipoBusca.SelectedIndex){
+				case 0:
+					// Procurar pelo artista
+					binarySearchResult = BinarySearch.BinarySearchDisplay(Array.ConvertAll(General.Artistas, a => a.Codigo).Where(a => a > 0).ToArray(), codigo);
+
+					if(binarySearchResult != -1){
+						editButton.Click += editArtista;
+						searchDefine.Text = "Nome do Artista: " + General.Artistas[binarySearchResult].NomeArtistico + " (" + General.Artistas[binarySearchResult].NomeVerdadeiro + ")";
+						dataGridLabel1.Text = "Músicas";
+						dataGridLabel2.Text = "Álbuns";
+
+						searchDefine.Visible = true;
+						editButton.Visible = true;
+						dataGridLabel1.Visible = true;
+						dataGridLabel2.Visible = true;
+						dataGridView1.Visible = true;
+						dataGridView2.Visible = true;
+
+						dataGridView1.ColumnCount = 2;
+						dataGridView1.Columns[0].Name = "Código";
+						dataGridView1.Columns[1].Name = "Título";
+						dataGridView1.Rows.Clear();
+						for (int i = 0; i < General.CancoesSize; i++) {
+							if (General.Cancoes[i].CodigoCompositor == codigo && General.Cancoes[i].CodigoArtistaPrimeiraGravacao == codigo) {
+								General.Cancao cancaoTemp = General.Cancoes[i];
+								dataGridView1.Rows.Add(cancaoTemp.Codigo, cancaoTemp.Titulo);
+							}
+						}
+
+						dataGridView2.ColumnCount = 4;
+						dataGridView2.Columns[0].Name = "Código";
+						dataGridView2.Columns[1].Name = "Título";
+						dataGridView2.Columns[2].Name = "Data de Lançamento";
+						dataGridView2.Columns[3].Name = "Número de Cópias Vendidas";
+						dataGridView2.Rows.Clear();
+						for (int i = 0; i < General.AlbumsSize; i++) {
+							for (int j = 0; j < General.Albums[i].ArtistasParticipantes.Length; j++) {
+								if (General.Albums[i].ArtistasParticipantes[j] == codigo) {
+									General.Album albumTemp = General.Albums[i];
+									dataGridView2.Rows.Add(albumTemp.Codigo, albumTemp.Titulo, albumTemp.DataLancamento.ToString("d"), albumTemp.NumeroCopiasVendidas.ToString());
+								}
+							}
+						}
+					} else {
+						MessageBox.Show("Nenhum artista encontrado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+
+					break;
+
+				case 1:
+					// Procurar pelo album
+					binarySearchResult = BinarySearch.BinarySearchDisplay(Array.ConvertAll(General.Albums, a => a.Codigo).Where(a => a > 0).ToArray(), codigo);
+
+					if (binarySearchResult != -1) {
+						editButton.Click += editAlbum;
+						searchDefine.Text = "Título do Album: " + General.Albums[binarySearchResult].Titulo;
+						dataGridLabel1.Text = "Músicas";
+						dataGridLabel2.Text = "Artistas";
+
+						searchDefine.Visible = true;
+						editButton.Visible = true;
+						dataGridLabel1.Visible = true;
+						dataGridLabel2.Visible = true;
+						dataGridView1.Visible = true;
+						dataGridView2.Visible = true;
+
+						dataGridView1.ColumnCount = 2;
+						dataGridView1.Columns[0].Name = "Código";
+						dataGridView1.Columns[1].Name = "Título";
+						dataGridView1.Rows.Clear();
+						for (int i = 0; i < General.Albums[binarySearchResult].CodigosMusicas.Length; i++) {
+							General.Cancao cancaoTemp = General.Cancoes[General.Albums[binarySearchResult].CodigosMusicas[i] - 1];
+							dataGridView1.Rows.Add(cancaoTemp.Codigo, cancaoTemp.Titulo);
+						}
+
+						dataGridView2.ColumnCount = 5;
+						dataGridView2.Columns[0].Name = "Código";
+						dataGridView2.Columns[1].Name = "Nome Verdadeiro";
+						dataGridView2.Columns[2].Name = "Nome Artístico";
+						dataGridView2.Columns[3].Name = "Data de Aniversário";
+						dataGridView2.Columns[4].Name = "Faz parte de uma banda?";
+						dataGridView2.Rows.Clear();
+						for (int i = 0; i < General.Albums[binarySearchResult].ArtistasParticipantes.Length; i++) {
+							General.Artista artistaTemp = General.Artistas[General.Albums[binarySearchResult].ArtistasParticipantes[i] - 1];
+							string banda;
+							if (artistaTemp.BandaBool)
+								banda = "Sim";
+							else
+								banda = "Não";
+							dataGridView2.Rows.Add(artistaTemp.Codigo, artistaTemp.NomeVerdadeiro, artistaTemp.NomeArtistico, artistaTemp.Aniversario.ToString("d"), banda, "Editar", "Remover");
+						}
+					} else {
+						MessageBox.Show("Nenhum álbum encontrado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+
+					break;
+
+				case 2:
+					// Procurar pela canção
+					binarySearchResult = BinarySearch.BinarySearchDisplay(Array.ConvertAll(General.Cancoes, a => a.Codigo).Where(a => a > 0).ToArray(), codigo);
+
+					if (binarySearchResult != -1) {
+						editButton.Click += editCancao;
+						searchDefine.Text = "Título da Canção: " + General.Cancoes[binarySearchResult].Titulo;
+						dataGridLabel1.Text = "Artistas";
+						dataGridLabel2.Text = "Álbuns";
+
+						searchDefine.Visible = true;
+						editButton.Visible = true;
+						dataGridLabel1.Visible = true;
+						dataGridLabel2.Visible = true;
+						dataGridView1.Visible = true;
+						dataGridView2.Visible = true;
+
+						dataGridView1.ColumnCount = 6;
+						dataGridView1.Columns[0].Name = "Código";
+						dataGridView1.Columns[1].Name = "Nome Verdadeiro";
+						dataGridView1.Columns[2].Name = "Nome Artístico";
+						dataGridView1.Columns[3].Name = "Data de Aniversário";
+						dataGridView1.Columns[4].Name = "Faz parte de uma banda?";
+						dataGridView1.Columns[5].Name = "Tipo";
+						dataGridView1.Rows.Clear();
+						string banda1, banda2;
+
+						General.Artista artistaTemp1 = General.Artistas[General.Cancoes[binarySearchResult].CodigoCompositor - 1];
+						banda1 = artistaTemp1.BandaBool ? "Sim" : "Não";
+						dataGridView1.Rows.Add(artistaTemp1.Codigo, artistaTemp1.NomeVerdadeiro, artistaTemp1.NomeArtistico, artistaTemp1.Aniversario.ToString("d"), banda1, "Compositor");
+
+						General.Artista artistaTemp2 = General.Artistas[General.Cancoes[binarySearchResult].CodigoArtistaPrimeiraGravacao - 1];
+						banda2 = artistaTemp2.BandaBool ? "Sim" : "Não";
+						dataGridView1.Rows.Add(artistaTemp2.Codigo, artistaTemp2.NomeVerdadeiro, artistaTemp2.NomeArtistico, artistaTemp2.Aniversario.ToString("d"), banda2, "Musicista");
+
+						dataGridView2.ColumnCount = 4;
+						dataGridView2.Columns[0].Name = "Código";
+						dataGridView2.Columns[1].Name = "Título";
+						dataGridView2.Columns[2].Name = "Data de Lançamento";
+						dataGridView2.Columns[3].Name = "Número de Cópias Vendidas";
+						dataGridView2.Rows.Clear();
+						for (int i = 0; i < General.AlbumsSize; i++) {
+							for (int j = 0; j < General.Albums[i].CodigosMusicas.Length; j++) {
+								if (General.Albums[i].CodigosMusicas[j] == codigo) {
+									General.Album albumTemp = General.Albums[i];
+									dataGridView2.Rows.Add(albumTemp.Codigo, albumTemp.Titulo, albumTemp.DataLancamento.ToString("d"), albumTemp.NumeroCopiasVendidas.ToString());
+								}
+							}
+						}
+					} else {
+						MessageBox.Show("Nenhuma canção encontrada", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+
+					break;
+			}
+		}
+
+		public void editArtista(object Sender, EventArgs e){
+			new ArtistaForm(binarySearchResult).ShowDialog();
+		}
+
+		public void editAlbum(object Sender, EventArgs e) {
+			new AlbumForm(binarySearchResult).ShowDialog();
+		}
+
+		public void editCancao(object Sender, EventArgs e) {
+			new CancaoForm(binarySearchResult).ShowDialog();
 		}
 	}
 }
