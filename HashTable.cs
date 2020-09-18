fc84d09deab61f396cb476620b28bb14ba459e13
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2020-09-18 08:25:28 -0300

    Update: HashTable.cs with QuadraticHashInsert and DoubleHashInsert

--- HashTable.cs
+++ HashTable.cs
@@ -1,44 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq.Expressions;
 using System.Net.NetworkInformation;
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Forms;
 
 namespace data_structure_project_record_company {
 	class HashTable {
-		public static void HashMain(){
-			String[] values = new string[50];
-			String str;
-			int hashCode;
+		class HashEntry {
+			int key;
+			string data;
 
-			String[] keys = new String[]{
-				"jpcarpanezi@gmail.com", "jp@proativa.info", "prof.raul@ifsp.edu.br", "jpcarpanezi@hotmail.com"
-			};	
+			public HashEntry(int key, String data) {
+				this.key = key;
+				this.data = data;
+			}
 
-			for (int i = 0; i < keys.Length; i++) {
-				str = keys[i];
+			public int GetKey() {
+				return key;
+			}
 
-				hashCode = Hash(str, values);
-				values[hashCode] = str;
+			public String GetData() {
+				return data;
 			}
 		}
 
-		public static int Hash(String s, String[] arr){
-			long total = 0;
-			char[] c;
-			c = s.ToCharArray();
+		const int maxSize = 10;
+		HashEntry[] table;
 
-			for (int i = 0; i <= c.GetUpperBound(0); i++) {
-				total += 11 * total + (int)c[i];
+		public HashTable() {
+			table = new HashEntry[maxSize];
+			for (int i = 0; i < maxSize; i++) {
+				table[i] = null;
+			}
+		}
+
+		private bool CheckOpenSpace() {
+			bool isOpen = false;
+			for (int i = 0; i < maxSize; i++) {
+				if (table[i] == null) {
+					isOpen = true;
+				}
 			}
 
-			total = total % arr.GetUpperBound(0);
+			return isOpen;
+		}
 
-			if(total < 0){
-				total += arr.GetUpperBound(0);
+		public String Retrive(int key) {
+			int hash = key % maxSize;
+			while (table[hash] != null && table[hash].GetKey() != key) {
+				hash = (hash + 1) % maxSize;
 			}
 
-			return (int)total;
+			if (table[hash] == null) {
+				return "Nada encontrado";
+			} else {
+				return table[hash].GetData();
+			}
+		}
+
+		public bool Remove(int key){
+			int hash = key % maxSize;
+
+			while (table[hash] != null && table[hash].GetKey() != key){
+				hash = (hash + 1) % maxSize;
+			}
+
+			if(table[hash] == null){
+				return false;
+			}else{
+				table[hash] = null;
+				return true;
+			}
+		}
+
+		public void Print(){
+			// Usando apenas para breakpoint de debug
+			for (int i = 0; i < table.Length; i++) {
+				if(table[i] == null && i <= maxSize){
+					continue;
+				}else{
+					table[i].GetData();
+				}
+			}
+		}
+
+		private int Hash1(int key){
+			return key % maxSize;
+		}
+
+		private int Hash2(int key){
+			return 5 - key % 5;
+		}
+
+		public void Insert(int key, String data) {
+			int hash = (key % maxSize);
+
+			if (!CheckOpenSpace()) {
+				MessageBox.Show("Tabela hash está cheia", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			while (table[hash] != null && table[hash].GetKey() != key) {
+				hash = (hash + 1) % maxSize;
+			}
+
+			table[hash] = new HashEntry(key, data);
+		}
+
+		public void QuadraticHashInsert(int key, String data){
+			int i = 0;
+			int hash = key % maxSize;
+
+			if(!CheckOpenSpace()){
+				MessageBox.Show("Tabela hash está cheia", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			while(table[hash] != null && table[hash].GetKey() != key){
+				i++;
+				hash = (hash + i * i) % maxSize;
+			}
+
+			if(table[hash] == null){
+				table[hash] = new HashEntry(key, data);
+				return;
+			}
+		}
+
+		public void DoubleHashInsert(int key, String data){
+			int hashVal = Hash1(key);
+			int stepSize = Hash2(key);
+
+			if (!CheckOpenSpace()) {
+				MessageBox.Show("Tabela hash está cheia", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			while(table[hashVal] != null && table[hashVal].GetKey() != key){
+				hashVal = (hashVal + stepSize * Hash2(key)) % maxSize;
+			}
+
+			table[hashVal] = new HashEntry(key, data);
+			return;
 		}
 	}
 }
